a54a0daf8383741945d57cf51a8060fc59f3ccd0
mflilian <mayhharamorais@gmail.com>
2021-08-19 18:50:51 -0300

    adds new solution

--- teams/start-tech/javascript/leap/leap.js
+++ teams/start-tech/javascript/leap/leap.js
@@ -3,7 +3,16 @@
 // convenience to get you started writing code faster.
 //
 
-export const isLeap = (year) => {
+ export const isLeap = (year) => {
   return (year % 4 === 0 && year % 100 !== 0) || 
          (year % 400 === 0)
+}; 
+
+// ou 
+
+export const isLeap = (year) => {
+  if ((!(year % 4) && (year % 100)) || !(year % 400)){
+    return true
+  } else {
+    return false}
 };
